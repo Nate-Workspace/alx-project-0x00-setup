9dc40cc06fe1b0a4723f404613081da8620f33b3
Nate-Workspace <nathanisrael.jobs@gmail.com>
2025-05-31 14:29:56 +0300

    Update - Task 6

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -10,26 +10,26 @@ const Landing: React.FC =  () => {
 
       <div className="space-y-2">
         <h2 className="text-lg font-semibold">Small Buttons</h2>
-        <Button title="Small - rounded-sm" className="text-sm rounded-sm" />
-        <Button title="Small - rounded-md" className="text-sm rounded-md" />
-        <Button title="Small - rounded-md" className="text-sm rounded-lg" />
-        <Button title="Small - rounded-full" className="text-sm rounded-full" />
+        <Button title="Small - rounded-sm" styles="text-sm rounded-sm" />
+        <Button title="Small - rounded-md" styles="text-sm rounded-md" />
+        <Button title="Small - rounded-md" styles="text-sm rounded-lg" />
+        <Button title="Small - rounded-full" styles="text-sm rounded-full" />
       </div>
 
       <div className="space-y-2">
         <h2 className="text-lg font-semibold">Medium Buttons</h2>
-        <Button title="Medium - rounded-sm" className="text-base rounded-sm" />
-        <Button title="Medium - rounded-md" className="text-base rounded-md" />
-        <Button title="Medium - rounded-md" className="text-base rounded-lg" />
-        <Button title="Medium - rounded-full" className="text-base rounded-full" />
+        <Button title="Medium - rounded-sm" styles="text-base rounded-sm" />
+        <Button title="Medium - rounded-md" styles="text-base rounded-md" />
+        <Button title="Medium - rounded-md" styles="text-base rounded-lg" />
+        <Button title="Medium - rounded-full" styles="text-base rounded-full" />
       </div>
 
       <div className="space-y-2">
         <h2 className="text-lg font-semibold">Large Buttons</h2>
-        <Button title="Large - rounded-sm" className="text-lg px-6 py-3 rounded-sm" />
-        <Button title="Large - rounded-md" className="text-lg px-6 py-3 rounded-md" />
-        <Button title="Large - rounded-md" className="text-lg px-6 py-3 rounded-lg" />
-        <Button title="Large - rounded-full" className="text-lg px-6 py-3 rounded-full" />
+        <Button title="Large - rounded-sm" styles="text-lg px-6 py-3 rounded-sm" />
+        <Button title="Large - rounded-md" styles="text-lg px-6 py-3 rounded-md" />
+        <Button title="Large - rounded-md" styles="text-lg px-6 py-3 rounded-lg" />
+        <Button title="Large - rounded-full" styles="text-lg px-6 py-3 rounded-full" />
       </div>
     </div>
 
